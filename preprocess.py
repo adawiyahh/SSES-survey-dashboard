376d59bae1fe397e2cc2871ea1bfc037f0be678c
nhusna01 <nrlhsn1310@gmail.com>
2025-12-12 02:48:25 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from configuration import GOOGLE_SHEET_URL
 
-@st.cache_data(ttl=15)   # refresh every 30 seconds
+@st.cache_data(ttl=15)   # refresh every 15 seconds
 def load_data():
     df = pd.read_csv(GOOGLE_SHEET_URL)
     return df
